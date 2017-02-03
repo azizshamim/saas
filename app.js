d33817ffb584fd17eac7df1144197f7dbfb70cbf
Aziz Shamim <azizshamim@gmail.com>
2017-02-03 11:38:38 -0600

    fix up various bugs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
 
 app.get('/ping', function (req, res) {
   res.send('PONG!')
@@ -7,10 +10,12 @@ app.get('/ping', function (req, res) {
 
 app.post('/sleep/:timer', function(req, res) {
   setTimeout( function () {
-    res.status(200).send(req.body);
+    console.log(req.body);
+    res.status(200).json(req.body);
   } , req.params.timer);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 });
